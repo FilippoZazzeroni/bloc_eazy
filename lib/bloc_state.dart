060fd0a2c41104faa7948cf4c2bbf4a9e24baff9
FilippoZazzeroni <55580351+FilippoZazzeroni@users.noreply.github.com>
2022-03-08 10:42:20 +0100

    simplified

--- lib/bloc_state.dart
+++ lib/bloc_state.dart
@@ -4,4 +4,18 @@ class BlocState {
   BlocState(this.rawValue);
 
   final String rawValue;
+
+  @override
+  bool operator ==(Object other) {
+    if (other is BlocState) {
+      return other.rawValue == rawValue;
+    }
+    return false;
+  }
+
+  @override
+  int get hashCode => BlocState._hash += 1;
+
+  static int _hash = 0;
+
 }
